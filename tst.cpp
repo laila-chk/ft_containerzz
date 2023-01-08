88b98579e7157b99502e407ea8af2b64bf9a1e37
Laila Chokri <lchokri@e2r5p10.1337.ma>
2023-01-08 22:35:26 +0100

    capacity methods..

--- tst.cpp
+++ tst.cpp
@@ -1,12 +1,16 @@
 
 #include "vector.hpp"
-
+#include <iostream>
 int main()
 {
+  // _allocater.MAX_SIZE()
+  try
+{
   std::vector<int>a(4,7);
-  ft::vector<int> b(a.begin(), a.end());
-  std::vector<int> c(a.begin(), a.end());
-  for (std::vector<int>::iterator it = c.begin(); it != c.end(); ++it)
-    std::cout << ' ' << *it;
-  
+  a.reserve(a.max_size() * 2);
+    std::cout<<"zaema donia hania" << std::endl;
+  }
+  catch (std::exception& e)
+  {std::cout << e.what() << std::endl;}
+    
 }
--- vector.hpp
+++ vector.hpp
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <memory>
 #include <vector>
+#include <stdexcept>
 
 
 namespace ft
@@ -19,7 +20,7 @@ namespace ft
       typedef Allocator                                     allocator_type;
       typedef typename allocator_type::reference            reference;
       typedef typename allocator_type::const_reference      const_reference;
-      typedef typename allocator_type::size_type            size_type;
+      typedef size_t                                        size_type;
       typedef typename allocator_type::difference_type      difference_type;
       typedef typename allocator_type::pointer              pointer;
       typedef typename allocator_type::const_pointer        const_pointer;
@@ -57,7 +58,15 @@ namespace ft
       void assign( InputIt first, InputIt last );
 
 */
-  void assign(size_type count, const value_type& value);
+      void assign(size_type count, const value_type& value);
+      size_type capacity() const;
+      bool empty() const;
+      size_type size() const;
+      size_type max_size() const;
+      void reserve( size_type new_cap );
+
+
+    /********************************************************************************/
   };
 
   template <class T, class Allocator >
@@ -65,7 +74,7 @@ namespace ft
   {}
 
   template <class value_type, class allocator_type>
-  vector<value_type, allocator_type >::vector (size_type n, const value_type& val ,const allocator_type& alloc) :_alloc(alloc), _begin(NULL), _end(NULL), _end_cap(NULL)
+  vector<value_type, allocator_type >::vector (size_type n, const value_type& val ,const allocator_type& alloc) :_alloc(alloc)
   {
     _begin = _alloc.allocate(n);
     for (size_type i; i < n; i++)
@@ -75,10 +84,55 @@ namespace ft
   }
   
   template <class value_type, class allocator_type>
-  void assign(size_type count, const value_type& value)
+  void vector<value_type, allocator_type >::assign(size_type count, const value_type& value)
   {
-
+    for (int i = 0; i < _end - _begin; i++)
+    {
+        this->destroy(_begin + i);
+      if (count > _end_cap - _begin)
+        this->deallocate(_begin + i);
+    }
+      if (count > _end_cap - _begin)
+        _begin = _alloc.allocate(count);
+      for (size_type i = 0; i < count; i++)
+        _alloc.construct(_begin + i, value);
+      _end = _begin + count;
+      _end_cap = _begin + count;
   }
+  template <class value_type, class allocator_type>
+  size_t vector<value_type, allocator_type>::size() const
+  {
+    return (_end - _begin);  
+  }
+  template <class value_type, class allocator_type>
+  size_t vector<value_type, allocator_type>::max_size() const
+  {
+    return (sizeof(size_t) / sizeof(value_type));  
+  }
+
+  template <class value_type, class allocator_type>
+  size_t vector<value_type, allocator_type>::capacity() const
+  {
+    return (_end_cap - _begin);  
+  }
+  
+  template <class value_type, class allocator_type>
+  bool vector<value_type, allocator_type>::empty() const
+  {
+    if (_end - _begin == 0)
+      return true;
+    return false;
+  }
+
+  template <class value_type, class allocator_type>
+  void vector<value_type, allocator_type>::reserve(size_t new_cap)
+  {
+    // if (new_cap > this->max_size())
+    //   throw std::length_error("") in case allocarot doesn't handle it ;
+
+    
+  }
+
 } //end of namespace ft
 // #include "vector.tpp"
 #endif 
